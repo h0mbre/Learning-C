8630379bb369c7834b9209fd3137711db67e33cf
h0mbre <47577114+h0mbre@users.noreply.github.com>
2019-09-14 19:47:44 -0400

    Create assignment2.c

---
+++ Assignment-2/assignment2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int integer = 5;
+  float floatvar = 3.14;
+  char string[] = "Hello, World!";
+
+  printf("%d is an integer!\n", integer);
+  printf("%f is a float!\n", floatvar);
+  printf("%s is a char!\n", string);
+
+}
