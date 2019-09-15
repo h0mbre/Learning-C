13dd014ddc0d8c498341d2afb61264bbff15a8d3
h0mbre <47577114+h0mbre@users.noreply.github.com>
2019-09-14 23:02:03 -0400

    Update assignment6.c

--- Assignment-6/assignment6.c
+++ Assignment-6/assignment6.c
@@ -1,40 +1,23 @@
 #include <stdio.h>
-#include <math.h>
 
 
 int main(void)
 {
-  //initialize vars and get their values
-  float a,b,c,x;
-  printf("Enter the value of variable \'A\': ");
-  scanf("%f", &a);
-  printf("Enter the value of variable \'B\': ");
-  scanf("%f", &b);
-  printf("Enter the value of variable \'C\': ");
-  scanf("%f", &c);
-
-  //start doing the math
-  float solution1, solution2;
-  solution1 = (-b + sqrt((b*b)-(4*a*c)))/(2*a);
-  solution2 = (-b - sqrt((b*b)-(4*a*c)))/(2*a);
-
-  //plug the solution back into the equation and check that it's correct: 0 = ax^2 + bx + c
-  if (((a*(solution1*solution1)) + (b*solution1) + c) == 0) 
-  {
-  	printf("The solution using the '+' operator is: %f\n", solution1);
-  }
-  else
-  {
-  	printf("The solution using the '+' operator is: %f, but you might want to double-check that...\n", solution1);
-  }
   
-  if (((a*(solution2*solution2)) + (b*solution2) + c) == 0) 
+  //initialize our vars and get values for them
+  int numerator, denominator;
+  printf("Enter a numerator: ");
+  scanf("%d", &numerator);
+  printf("Enter a denominator: ");
+  scanf("%d", &denominator);
+
+  if (numerator % denominator == 0)
   {
-  	printf("[The solution using the '-' operator is: %f", solution2);
+  	printf("There is NOT a remainder!");
   }
   else
   {
-  	printf("The solution using the '+' operator is: %f, but you might want to double check that...\n", solution2);
+  	printf("There is a remainder!");
   }
 
   return 0;
