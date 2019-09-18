ea3b25ea350d3791340f0ce969047b2ab441271c
h0mbre <47577114+h0mbre@users.noreply.github.com>
2019-09-17 20:09:12 -0400

    Update assignment20.c

--- Assignment-20/assignment20.c
+++ Assignment-20/assignment20.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//define structure
 struct struct_name
 {
 	int integer;
 	char character;
 	float floatie;
+//give an instance of the structure the name 'example'
 } example;
 
 int main (void)
 {
+	//use the . notation to refer to the members of the 'example' instance of the 'struct_name' struct
 	example.integer = 50;
 	example.character = 'W';
 	example.floatie = 3.14;
