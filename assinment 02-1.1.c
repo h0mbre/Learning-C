5d3447c394cf7e4d9aeb5f84a90ef5cd1bc46878
sid050 <60535097+sid050@users.noreply.github.com>
2020-02-05 21:49:31 +0530

    input from users

---
+++ assinment 02-1.1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+//author: Siddhnta Das
+//date of creation: 05/02/20
+int main()
+{
+int i;
+printf("enter a number \n"); //input an  integer number
+scanf("%d",&i);
+float j;
+printf("enter a number \n");              //input a decimal number
+scanf("%f",&j);
+char ch;
+printf("enter the character \n");         //input a character
+scanf("%s",&ch);
+printf("%d is an integer  \n",i);
+printf("%f is a float \n",j);
+printf("%s is a character \n",&ch);
+
+}
