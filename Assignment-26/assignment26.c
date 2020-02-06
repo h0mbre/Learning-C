298eb4d73cdd002e48b11a89f7f643be7a76db3b
Andreas Schickedanz <as@bornageek.com>
2020-02-06 23:03:56 +0100

    Assignment 26: Add missing error handling

--- Assignment-26/assignment26.c
+++ Assignment-26/assignment26.c
@@ -1,11 +1,11 @@
 #include <netinet/in.h>
+#include <errno.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
 int main(void) {
-
     //creating variable listen_sock which is returned by socket() as we need it for the bind(), listen(), and accept() calls
     int listen_sock = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -19,18 +19,52 @@ int main(void) {
     bind(listen_sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
 
     //0 here is our specified backlog, (we dont have one)
-    listen(listen_sock, 0);
+    if(listen(listen_sock, 0) == -1) {
+        perror("Failed to listen on sock");
+        close(listen_sock);
+        return errno;
+    }
 
     //creating new var conn_sock which is returned from accept() and needed for the dup2() call which duplicates this for stdin, stdout, and stderr
     int conn_sock = accept(listen_sock, NULL, NULL);
+    if(conn_sock == -1) {
+         perror("Failed to accept connection");
+         close(listen_sock);
+         return errno;
+    }
 
     // Our fifth syscall, dup2(), is used 3 times
-    dup2(conn_sock, 0);
-    dup2(conn_sock, 1);
-    dup2(conn_sock, 2);
+    if(dup2(conn_sock, 0) == -1) {
+         perror("Failed to create copy of stdin");
+         close(conn_sock);
+         close(listen_sock);
+         return errno;
+    }
+
+    if(dup2(conn_sock, 1) == -1) {
+         perror("Failed to create copy of stdout");
+         close(conn_sock);
+         close(listen_sock);
+         return errno;
+    }
+
+    if(dup2(conn_sock, 2) == -1) {
+         perror("Failed to create copy of stderr");
+         close(conn_sock);
+         close(listen_sock);
+         return errno;
+    }
 
     // Our final syscall is execve(), which runs a program fed to it as a string
-    execve("/bin/sh", NULL, NULL);
+    if(execve("/bin/sh", NULL, NULL) == -1) {
+         perror("Failed to start shell");
+         close(conn_sock);
+         close(listen_sock);
+         return errno;
+    }
+
+    close(conn_sock);
+    close(listen_sock);
 
     return 0;
 }
