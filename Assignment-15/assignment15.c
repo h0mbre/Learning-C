a9c358c53b6bed5e856cf00e9c089e0e9a0e23c1
Andreas Schickedanz <as@bornageek.com>
2020-02-06 22:00:11 +0100

    Assignment 15: Use %p instead of %d to print pointer

--- Assignment-15/assignment15.c
+++ Assignment-15/assignment15.c
@@ -10,10 +10,10 @@ int main(void)
   printf("The value of int variable is: %d\n", variable);
 
   //since we assigned the pointer to 'variable', our pointer's value is now the memory address of 'variable'
-  printf("The value of the pointer to the int variable is: %d\n", ptr);
+  printf("The value of the pointer to the int variable is: %p\n", ptr);
 
   //using the ampersand before variable means we're printing it's memory address instead of it's value
-  printf("The memory address of the int variable is: %d\n", &variable);
+  printf("The memory address of the int variable is: %p\n", &variable);
 
   //using the asterisk means we will print the value held at the memory address pointed to by the pointer
   printf("The value held at the memory location that the pointer is pointing to is: %d\n", *ptr);
