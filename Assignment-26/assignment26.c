28dc2dcaec5f165da9b7d792fc4ddaa9db2169a9
Andreas Schickedanz <as@bornageek.com>
2020-02-06 22:34:45 +0100

    Assignment 26: Fix formatting and remove blank whitespaces

--- Assignment-26/assignment26.c
+++ Assignment-26/assignment26.c
@@ -4,16 +4,16 @@
 
 int main(void) {
 
-    //creating variable listen_sock which is returned by socket() as we need it for the bind(), listen(), and accept() calls 
+    //creating variable listen_sock which is returned by socket() as we need it for the bind(), listen(), and accept() calls
     int listen_sock = socket(AF_INET, SOCK_STREAM, 0);
 
-    // Building a 'struct' which consists of AF_INET, the interface we want to listen on (all), and a port number to bind on. This entire entity will be referenced in arguments for the next syscall: bind()    
+    // Building a 'struct' which consists of AF_INET, the interface we want to listen on (all), and a port number to bind on. This entire entity will be referenced in arguments for the next syscall: bind()
     struct sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;           
-    server_addr.sin_addr.s_addr = INADDR_ANY;  
-    server_addr.sin_port = htons(5555);        
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(5555);
 
-    // Our second syscall, and perhaps the most complicated: bind() 
+    // Our second syscall, and perhaps the most complicated: bind()
     bind(listen_sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
 
     //0 here is our specified backlog, (we dont have one)
