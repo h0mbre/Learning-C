061b5e944cd19e8d6757f03dce56c282b54d975f
CAESIUS_TIM <60285058+CAESIUS-TIM@users.noreply.github.com>
2023-07-28 10:43:14 +0800

    :art: Assignment 9: refactor &  add comments

--- Assignment-09/assignment9.c
+++ Assignment-09/assignment9.c
@@ -4,12 +4,15 @@
 int main(int argc, char *argv[])
 {
   
-  if(argc < 3 || argc > 3)
+  if(argc != 3) 
   {
   	printf("Usage: ./assignment9 Firstname Lastname");
   }
   else
   {
+    // 0: the name of the program
+    // 1: Firstname
+    // 2: Lastname
   	printf("Hello, %s %s", argv[1], argv[2]);
   }
   return 0;
