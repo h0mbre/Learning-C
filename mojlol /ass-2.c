89edbb17c608554ffe1ab01262a41587953e716a
jusufnovalic <117160230+jusufnovalic@users.noreply.github.com>
2023-07-31 17:44:16 +0200

    ass-2

---
+++ mojlol /ass-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    int x=5;
+    float y= 2.5;
+    char c = 'a';
+    printf("%d is a integer!\n%f is a float!\n%c is a char!\n",x,y,c);
+    return 0; 
+
+
+}
