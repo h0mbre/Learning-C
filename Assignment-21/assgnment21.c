dbc5bffe3a858b197ddbc8ad430a55f68dd94c7c
Houssem-eddine Lahmer <Houssemeddine.lahmar@etudiant-enit.utm.tn>
2024-03-26 02:18:51 +0100

    Refactored loop logic to simplify selection of employee and removed redundant if conditions, enhancing code readability.

--- Assignment-21/assgnment21.c
+++ Assignment-21/assgnment21.c
@@ -2,59 +2,54 @@
 #include <string.h>
 
 struct data {
-	char first[20];
-	char last[20];
-	int employee_id;
-	int last_4_SSN;
-	char title[20];
+    char first[20];
+    char last[20];
+    int employee_id;
+    int last_4_SSN;
+    char title[20];
 };
 
 struct employees {
-	struct data employee1;
-	struct data employee2;
+    struct data employee1;
+    struct data employee2;
 } engineering_dept;
 
-int main(void)
-{
-	//scanf the input into the correct variable
-	//engineering_dept = name of the instance of the 'employees' struct
-	//employee1 = member of the 'employees' struct
-	//first = member of the 'data' struct
-	//notice that employee1 and employee2 are 'data' struct instances themselves
-	struct data *employee;
-    for(int i = 0; i < 2; i++) {
-		if(i > 0)
-			employee = &engineering_dept.employee2;
+int main(void) {
+    struct data *employee;
+    
+    // Loop to input data for each employee
+    for (int i = 0; i < 2; i++) {
+        // Select the appropriate employee based on the loop index
+        employee = (i == 0) ? &engineering_dept.employee1 : &engineering_dept.employee2;
 
-		printf("Enter the employee's first name: ");
-		scanf("%s", &employee->first);
+        printf("Enter the employee's first name: ");
+        scanf("%s", employee->first);
 
-		printf("Enter the employee's last name: ");
-		scanf("%s", &employee->last);
+        printf("Enter the employee's last name: ");
+        scanf("%s", employee->last);
 
-		printf("Enter the employee's ID number: ");
-		scanf("%d", &employee->employee_id);
+        printf("Enter the employee's ID number: ");
+        scanf("%d", &employee->employee_id);
 
-		printf("Enter the last four digits of the employee's SSN: ");
-		scanf("%d", &employee->last_4_SSN);
+        printf("Enter the last four digits of the employee's SSN: ");
+        scanf("%d", &employee->last_4_SSN);
 
-		printf("Enter the employee's job title (do not include the word \'Engineer\'): ");
-		scanf("%s", &employee->title);
+        printf("Enter the employee's job title (do not include the word 'Engineer'): ");
+        scanf("%s", employee->title);
 
-		puts("");
-	}
+        puts("");
+    }
 
-	//print all the info we just stored for each employee
-	for(int i = 0; i < 2; i++) {
-		struct data *employee = &engineering_dept.employee1;
-		if(i > 0)
-			employee = &engineering_dept.employee2;
+    // Print all the information stored for each employee
+    for (int i = 0; i < 2; i++) {
+        // Select the appropriate employee based on the loop index
+        employee = (i == 0) ? &engineering_dept.employee1 : &engineering_dept.employee2;
 
-		printf("\nEmployee information for %s %s: ", employee->first, employee->last);
-		printf("\nID: %d", employee->employee_id);
-		printf("\nSSN: %d", employee->last_4_SSN);
-		printf("\nTitle: %s Engineer\n", employee->title);
-	}
+        printf("\nEmployee information for %s %s: ", employee->first, employee->last);
+        printf("\nID: %d", employee->employee_id);
+        printf("\nSSN: %d", employee->last_4_SSN);
+        printf("\nTitle: %s Engineer\n", employee->title);
+    }
 
-	return 0;
+    return 0;
 }
